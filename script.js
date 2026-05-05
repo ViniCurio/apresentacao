72b2b8507502bbd21dfa66872f8b67d60a8882d6
Vinicius <vinicurio@hotmail.com>
2026-05-05 18:12:01 -0300

    First commit

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
- const aboutSection = document.getElementById('about');
+const aboutSection = document.getElementById('about');
 const aboutButton = document.getElementById('aboutButton');
 
 aboutButton.addEventListener('click', () => {
   aboutSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+
+const heroSection = document.getElementById('hero');
+const topButton = document.getElementById('topButton');
+
+topButton.addEventListener('click', () => {
+  heroSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
